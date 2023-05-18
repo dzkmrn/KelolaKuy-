7ad25f0dc324ba7e59a55527f847f391ba473daa
dzkmrn <2141720076@student.polinema.ac.id>
2023-05-19 06:56:57 +0700

    Penyempurnaan Login dan Register Page 5/19

--- KelolaKuy!/resources/views/auth/login.blade.php
+++ KelolaKuy!/resources/views/auth/login.blade.php
@@ -33,7 +33,8 @@
                                         <label for="username" class="form-label">{{ __('Username') }}</label>
                                         <div class="input-group has-validation">
                                             <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                            <input type="text" name="username" value="{{ old('username') }}" required autocomplete="username" class="form-control" id="username" required autocomplete="username" autofocus>
+                                            <input type="text" name="username" value="{{ old('username') }}" class="form-control" id="username" required autocomplete="username" autofocus>
+                                            <div class="invalid-feedback">Please enter your username.</div>
                                             @error('username')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
