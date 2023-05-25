7bf589f6341b9e7027f8dea3172267b1f652d737
syahla31 <syahlasyafiqah31@gmail.com>
2023-05-25 12:30:23 +0700

    Membuat seeder data master (jenis) 25/05

---
+++ KelolaKuy!/database/seeders/AlatSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AlatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id_alat'=>01,
+                'id_kategori'=>01,
+                'id_jenis'=>01,
+                'nama_alat'=>'Peralatan Makan',
+                'foto_alat'=>'',
+                'merek_alat'=>'',
+                'stok_alat'=>''
+            ],
+            [
+                'id_alat'=>01,
+                'id_kategori'=>01,
+                'id_jenis'=>01,
+                'nama_alat'=>'Peralatan Makan',
+                'foto_alat'=>'',
+                'merek_alat'=>'',
+                'stok_alat'=>''
+            ],
+            [
+                'id_alat'=>01,
+                'id_kategori'=>01,
+                'id_jenis'=>01,
+                'nama_alat'=>'Peralatan Makan',
+                'foto_alat'=>'',
+                'merek_alat'=>'',
+                'stok_alat'=>''
+            ],
+            [
+                'id_alat'=>01,
+                'id_kategori'=>01,
+                'id_jenis'=>01,
+                'nama_alat'=>'Peralatan Makan',
+                'foto_alat'=>'',
+                'merek_alat'=>'',
+                'stok_alat'=>''
+            ],
+            [
+                'id_alat'=>01,
+                'id_kategori'=>01,
+                'id_jenis'=>01,
+                'nama_alat'=>'Peralatan Makan',
+                'foto_alat'=>'',
+                'merek_alat'=>'',
+                'stok_alat'=>''
+            ],
+        ];
+        DB::table('jenis')->insert($data);
+    }
+}
